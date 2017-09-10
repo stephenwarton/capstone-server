5123619a90b791cdb8b40af2815d989c23c57dd8
Stephen Warton <stephen.warton@gmail.com>
2017-09-10 16:41:08 -0600

    initial commit

---
+++ api/router.js
@@ -0,0 +1,8 @@
+const queries = require('../db/queries');
+const express = require('express');
+const valid = require('./validate');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+module.exports = router;
